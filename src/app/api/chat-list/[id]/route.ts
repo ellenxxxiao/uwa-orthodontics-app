dba23a43a440b38f61ca9aedf252b64a6d10c3b7
ellenxxxiao <ellenxiao0412@gmail.com>
2024-07-29 14:43:24 +0800

    OCA-123 added type; endpoint created. sample usage above code

--- src/app/api/chat-list/[id]/route.ts
+++ src/app/api/chat-list/[id]/route.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 import { HttpStatusCode } from "axios";
+import { ChatListItem } from "@/types/chat";
 
 const prisma = new PrismaClient();
+
 
 // GET. Example: http://localhost:3000/api/chat-list/0d7e9ae9-dcd9-4bc9-8908-1715778cfaf9
 export async function GET(
@@ -11,21 +13,65 @@ export async function GET(
   const userId = params.id;
 
   try {
-    const chats = await prisma.message.findMany({
+    const messages = await prisma.message.findMany({
+      relationLoadStrategy: 'join',
+      select: {
+        id: true,
+        text: true,
+        sentAt: true,
+        Sender: {
+          select: {
+            id: true,
+            firstName: true,
+            lastName: true,
+          }
+        },
+        Receiver: {
+          select: {
+            id: true,
+            firstName: true,
+            lastName: true,
+          }
+        }
+      },
       where: {
         OR: [{ senderId: userId }, { receiverId: userId }]
       },
-      orderBy: {}
+      orderBy: {
+        sentAt: 'desc'
+      }
     });
 
-    // if (!chats) {
-    //   return new Response(JSON.stringify({ error: "User not found" }), {
-    //     status: HttpStatusCode.NotFound
-    //   });
-    // }
-    // return new Response(JSON.stringify(user), {
-    //   status: HttpStatusCode.Ok
-    // });
+    
+    // Map to store the last messages with each contact
+    const chatMap = new Map<string, ChatListItem>();
+
+    messages.forEach(message => {
+      const contactId = message.Sender.id === userId ? message.Receiver.id : message.Sender.id;
+      const contact = message.Sender.id === userId ? message.Receiver : message.Sender;
+
+      if (!chatMap.has(contactId)) {
+        chatMap.set(contactId, {
+          contactUser: {
+            id: contact.id,
+            firstName: contact.firstName,
+            lastName: contact.lastName
+          },
+          lastMessage: {
+            text: message.text,
+            sentAt: message.sentAt
+          }
+        });
+      }
+    }
+    )
+
+    const chatList = Array.from(chatMap.values());
+
+    return new Response(JSON.stringify(chatList), {
+      status: HttpStatusCode.Ok
+    });
+
   } catch (error) {
     // console.error("Error accessing database:", error);
     return new Response(JSON.stringify({ error }), {
---
+++ src/types/chat.ts
@@ -0,0 +1,11 @@
+export type ChatListItem = {
+    contactUser: {
+      id: string;
+      firstName: string;
+      lastName: string;
+    };
+    lastMessage: {
+      text: string;
+      sentAt: Date;
+    };
+  };
