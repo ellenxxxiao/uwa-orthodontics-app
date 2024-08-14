34267b000b8d3dbfbc024a658acc0d89bc77eba1
qiaoster <zimuzhang1@gmail.com>
2024-08-15 01:42:08 +0800

    applied prettier as instructed by github checks

--- src/app/reminder-list/page.tsx
+++ src/app/reminder-list/page.tsx
@@ -13,7 +13,10 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 enum RepeatPeriod {
-  Weekly, Monthly, Yearly, Custom
+  Weekly,
+  Monthly,
+  Yearly,
+  Custom
 }
 
 interface Reminder {
@@ -23,48 +26,53 @@ interface Reminder {
   patientName: string; // TODO: change to patientID when we decide how to do patientID
   repeatPeriod: RepeatPeriod;
   startDate: string; // TODO: change to a real date type
-  endDate: string;   // TODO: change to a real date type
+  endDate: string; // TODO: change to a real date type
   timeOfDay: string; // TODO: change to a real time type
   messageToPatient: string;
 }
 
 export default function ReminderList() {
-  const emptyReminder: Reminder =
-    { reminderName: 'New Reminder',
-      reminderID: 0,
-      notesToClinician: 'Notes to yourself',
-      patientName: 'Put Patient\'s name here',
-      repeatPeriod: RepeatPeriod.Weekly,
-      startDate: 'put starting date here',
-      endDate: 'put end date here',
-      timeOfDay: 'when in the day to send this reminder',
-      messageToPatient: 'message to display to patient when they receive this reminder'
-    };
+  const emptyReminder: Reminder = {
+    reminderName: "New Reminder",
+    reminderID: 0,
+    notesToClinician: "Notes to yourself",
+    patientName: "Put Patient's name here",
+    repeatPeriod: RepeatPeriod.Weekly,
+    startDate: "put starting date here",
+    endDate: "put end date here",
+    timeOfDay: "when in the day to send this reminder",
+    messageToPatient:
+      "message to display to patient when they receive this reminder"
+  };
 
   const reminders: Reminder[] = [
-    { reminderName: 'Test reminder 1',
+    {
+      reminderName: "Test reminder 1",
       reminderID: 123,
-      notesToClinician: 'this is test reminder #1',
-      patientName: 'Zimu Zhang',
+      notesToClinician: "this is test reminder #1",
+      patientName: "Zimu Zhang",
       repeatPeriod: RepeatPeriod.Weekly,
-      startDate: '2020-02-02',
-      endDate: '2024-01-17',
-      timeOfDay: '9:00AM',
-      messageToPatient: 'this is a hard-coded message for reminder #1'},
-    { reminderName: 'Test reminder 2',
+      startDate: "2020-02-02",
+      endDate: "2024-01-17",
+      timeOfDay: "9:00AM",
+      messageToPatient: "this is a hard-coded message for reminder #1"
+    },
+    {
+      reminderName: "Test reminder 2",
       reminderID: 456,
-      notesToClinician: 'this is test reminder #2',
-      patientName: 'Alian Haidar',
+      notesToClinician: "this is test reminder #2",
+      patientName: "Alian Haidar",
       repeatPeriod: RepeatPeriod.Monthly,
-      startDate: '1999-09-09',
-      endDate: '2000-01-01',
-      timeOfDay: '10:45PM',
-      messageToPatient: 'this is a hard-coded message for reminder #2'}
+      startDate: "1999-09-09",
+      endDate: "2000-01-01",
+      timeOfDay: "10:45PM",
+      messageToPatient: "this is a hard-coded message for reminder #2"
+    }
   ];
 
-  const [editing, setEditing] = useState<boolean>(false)
+  const [editing, setEditing] = useState<boolean>(false);
 
-  const handleReminderCardClick = (i:number) => {
+  const handleReminderCardClick = (i: number) => {
     setEditing(true);
   };
 
@@ -80,8 +88,13 @@ export default function ReminderList() {
     setEditing(false);
   };
 
-  const handleEdit = (event: ChangeEvent<HTMLInputElement>, newReminder: Reminder) => {
-    const index = reminders.findIndex(item => item.reminderID === newReminder.reminderID);
+  const handleEdit = (
+    event: ChangeEvent<HTMLInputElement>,
+    newReminder: Reminder
+  ) => {
+    const index = reminders.findIndex(
+      (item) => item.reminderID === newReminder.reminderID
+    );
     if (index === -1) {
       reminders.push(newReminder);
     } else {
@@ -91,60 +104,104 @@ export default function ReminderList() {
 
   return (
     <>
-    { editing ? (
-      <div className="flex h-screen flex-col">
-      <Header
-          title="Edit Reminder"
-          type="primary"
-          iconLeft={
-            <IconButton aria-label="back" className="bg-base-100">
-              <LuStepBack className="text-primary" size={30} strokeWidth={1.3} onClick={handleCancelClick}/>
-            </IconButton>
-          }
-          iconRight={
+      {editing ? (
+        <div className="flex h-screen flex-col">
+          <Header
+            title="Edit Reminder"
+            type="primary"
+            iconLeft={
               <IconButton aria-label="back" className="bg-base-100">
-                <LuSave className="text-primary" size={30} strokeWidth={1.3} onClick={handleSaveClick}/>
+                <LuStepBack
+                  className="text-primary"
+                  size={30}
+                  strokeWidth={1.3}
+                  onClick={handleCancelClick}
+                />
               </IconButton>
-          }/>
-        <AddReminder />
-      <Footer />
-      </div>
-    ) : (
-      <div className="flex h-screen flex-col">
-        <Header
-          type="primary"
-          iconLeft={<LuSearch size={30} strokeWidth={1.3} className="text-primary"/>}
-          iconRight={<LuPlusCircle size={30} strokeWidth={1.3} className="text-primary" onClick={handleAddNewButton}/>}
-          title="Reminders"/>
-        <div className="w-full flex-1 flex flex-col overflow-hidden bg-base-100">
-
-        {/* main */}
-        <div className="flex-1 overflow-y-auto p-4">
-          {(() => {
-            const reminderCards = [];
-            for (let i = 0; i < reminders.length; i++) {
-              reminderCards.push(
-                <div className="flex items-center mb-4 p-4 bg-white rounded-lg shadow relative cursor-pointer" onClick={()=>handleReminderCardClick(i)} onKeyDown={()=>handleReminderCardClick(i)}>
-                  <div className="absolute left-6 w-1 h-16 bg-blue-500"></div>
-                  <div className="w-16 h-16 bg-gray-200 rounded-full mr-4 ml-4 relative">
-                    <img src="/path/to/user/image" alt="User" className="w-full h-full object-cover rounded-full" />
-                  </div>
-                  <div className="flex-1">
-                    <div className="flex justify-between items-center">
-                      <h3 className="font-bold text-lg text-black">{reminders[i].patientName}</h3>
-                      <span className="py-2 px-4 rounded-full bg-blue-500 text-sm">{"ALGINERS"}</span>
-                    </div>
-                    <p className="text-gray-600">{reminders[i].startDate + " to " + reminders[i].endDate + ",  " + reminders[i].timeOfDay + ",  " + reminders[i].repeatPeriod}</p>
-                    <p className="text-gray-600">{reminders[i].messageToPatient}</p>
-                  </div>
-                </div>);}
-            return reminderCards;
-          })()}
+            }
+            iconRight={
+              <IconButton aria-label="back" className="bg-base-100">
+                <LuSave
+                  className="text-primary"
+                  size={30}
+                  strokeWidth={1.3}
+                  onClick={handleSaveClick}
+                />
+              </IconButton>
+            }
+          />
+          <AddReminder />
+          <Footer />
         </div>
-      </div>
-      <Footer />
-    </div>
-    )}
+      ) : (
+        <div className="flex h-screen flex-col">
+          <Header
+            type="primary"
+            iconLeft={
+              <LuSearch size={30} strokeWidth={1.3} className="text-primary" />
+            }
+            iconRight={
+              <LuPlusCircle
+                size={30}
+                strokeWidth={1.3}
+                className="text-primary"
+                onClick={handleAddNewButton}
+              />
+            }
+            title="Reminders"
+          />
+          <div className="flex w-full flex-1 flex-col overflow-hidden bg-base-100">
+            {/* main */}
+            <div className="flex-1 overflow-y-auto p-4">
+              {(() => {
+                const reminderCards = [];
+                for (let i = 0; i < reminders.length; i++) {
+                  reminderCards.push(
+                    <div
+                      className="relative mb-4 flex cursor-pointer items-center rounded-lg bg-white p-4 shadow"
+                      onClick={() => handleReminderCardClick(i)}
+                      onKeyDown={() => handleReminderCardClick(i)}
+                    >
+                      <div className="absolute left-6 h-16 w-1 bg-blue-500"></div>
+                      <div className="relative ml-4 mr-4 h-16 w-16 rounded-full bg-gray-200">
+                        <img
+                          src="/path/to/user/image"
+                          alt="User"
+                          className="h-full w-full rounded-full object-cover"
+                        />
+                      </div>
+                      <div className="flex-1">
+                        <div className="flex items-center justify-between">
+                          <h3 className="text-lg font-bold text-black">
+                            {reminders[i].patientName}
+                          </h3>
+                          <span className="rounded-full bg-blue-500 px-4 py-2 text-sm">
+                            {"ALGINERS"}
+                          </span>
+                        </div>
+                        <p className="text-gray-600">
+                          {reminders[i].startDate +
+                            " to " +
+                            reminders[i].endDate +
+                            ",  " +
+                            reminders[i].timeOfDay +
+                            ",  " +
+                            reminders[i].repeatPeriod}
+                        </p>
+                        <p className="text-gray-600">
+                          {reminders[i].messageToPatient}
+                        </p>
+                      </div>
+                    </div>
+                  );
+                }
+                return reminderCards;
+              })()}
+            </div>
+          </div>
+          <Footer />
+        </div>
+      )}
     </>
   );
 }
