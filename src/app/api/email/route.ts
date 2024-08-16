9630bc496047ef9aea011885a45bd4c9702b0709
Sierra <71174590+alian1001@users.noreply.github.com>
2024-08-16 11:52:59 +0800

    Email Template

---
+++ src/app/api/email/route.ts
@@ -0,0 +1,23 @@
+import { EmailTemplate } from '../../components/emailTemplate/email-template';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['alian.haidar01@gmail.com'],
+      subject: 'Hello world',
+      react: EmailTemplate({ firstName: 'Alian' }),
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
