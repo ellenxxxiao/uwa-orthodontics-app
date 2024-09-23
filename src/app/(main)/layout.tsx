c1c33829f367bff4fbb1ea3456a7b604219e9fb5
Ellen Xiao <ellenxiao0412@gmail.com>
2024-09-23 22:15:08 +0800

    separated layout logic

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,27 @@
+import { ClerkLoading, ClerkLoaded } from "@clerk/nextjs";
+
+import NavBar from "@/components/main/NavBar";
+
+export default function MainLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="flex h-screen bg-base-100">
+        <ClerkLoading>
+          <div className="flex h-full w-full items-center justify-center">
+            <p className="text-xl">Loading...</p>
+          </div>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <NavBar />
+          <main className="my-4 mr-4 flex-1 overflow-auto rounded-xl shadow-standard md:max-2xl:m-8 md:max-2xl:ml-0">
+            {children}
+          </main>
+        </ClerkLoaded>
+      </body>
+    </html>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { ClerkProvider, ClerkLoading, ClerkLoaded } from "@clerk/nextjs";
-
-import NavBar from "@/components/main/NavBar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "OrthoChat",
@@ -15,9 +10,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <ClerkProvider
       appearance={{
@@ -30,19 +25,7 @@ export default function RootLayout({
       }}
     >
       <html lang="en">
-        <body className="flex h-screen bg-base-100">
-          <ClerkLoading>
-            <div className="flex h-full w-full items-center justify-center">
-              <p className="text-xl">Loading...</p>
-            </div>
-          </ClerkLoading>
-          <ClerkLoaded>
-            <NavBar />
-            <main className="my-4 mr-4 flex-1 overflow-auto rounded-xl shadow-standard md:max-2xl:m-8 md:max-2xl:ml-0">
-              {children}
-            </main>
-          </ClerkLoaded>
-        </body>
+        <body>{children}</body>
       </html>
     </ClerkProvider>
   );
