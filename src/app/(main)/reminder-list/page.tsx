94a9db0ea50edad239680b7b1cf0b81b0ca33e80
Ellen Xiao <ellenxiao0412@gmail.com>
2024-10-16 18:29:46 +0800

    fetch reminders from endpoint

--- src/app/(main)/reminder-list/page.tsx
+++ src/app/(main)/reminder-list/page.tsx
@@ -1,56 +1,70 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useUser } from "@clerk/nextjs"; // Clerk hook to get the logged-in user
+import { useRouter } from "next/navigation";
+import { useUser } from "@clerk/nextjs";
+import { ReminderType, RepeatType } from "@prisma/client";
 import { LuPlusCircle } from "react-icons/lu";
+
 import EditReminderModal from "@/components/main/EditReminderModal";
 import Header from "@/components/main/Header";
 import ReminderCard from "@/components/main/ReminderCard";
 import type { ReminderItem } from "@/types/reminder";
 
 export default function ReminderList() {
+  const { isSignedIn, isLoaded } = useUser();
   const [isOpen, setIsOpen] = useState(false);
+  const [reminders, setReminders] = useState<ReminderItem[]>([]);
   const [selectedReminder, setSelectedReminder] = useState<ReminderItem | null>(
     null
   );
-  const [reminders, setReminders] = useState<ReminderItem[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
 
-  const { user } = useUser(); // Get the current logged-in user
+  const router = useRouter();
 
-  // Print the user object to the console for debugging purposes
+  if (isLoaded && !isSignedIn) {
+    router.push("/sign-in");
+    return;
+  }
+
   useEffect(() => {
-    if (user) {
-      console.log("Logged-in user:", user);
-    }
-  }, [user]);
-
-  // Fetch reminders for the logged-in user via the API
-  useEffect(() => {
-    const fetchReminders = async () => {
-      if (!user) return; // Wait until the user is loaded
-
+    async function fetchReminders() {
       try {
-        const response = await fetch(`/api/reminder/1`); // Fetch reminders, 1 is just set, as API already has the user ID
+        const response = await fetch("/api/reminder/");
         if (!response.ok) {
-          throw new Error("Failed to fetch reminders");
+          throw new Error("Network response was not ok");
         }
-        const data = await response.json();
+        const data: ReminderItem[] = await response.json(); // Type assertion
         setReminders(data);
       } catch (error) {
-        if (error instanceof Error) {
-          setError(error.message);
-        } else {
-          setError("An unknown error occurred");
-        }
-      } finally {
-        setLoading(false);
+        throw new Error("Failed to fetch reminders");
       }
-    };
+    }
 
-    fetchReminders();
-  }, [user]);
+    isLoaded && fetchReminders();
+  }, [isLoaded, isSignedIn, router]);
+
+  // mock list of reminders
+  // const reminders: ReminderItem[] = [
+  //   {
+  //     reminderId: 1,
+  //     patientName: "John Doe",
+  //     startDate: "2022-10-01 11:11:00",
+  //     endDate: "2022-10-31",
+  //     intervalInDays: 7,
+  //     reminderType: ReminderType.ALIGNER,
+  //     description: "ReminderType.ALIGNER",
+  //     repeat: RepeatType.WEEKLY
+  //   },
+  //   {
+  //     reminderId: 2,
+  //     patientName: "Ellen Xiao",
+  //     startDate: "2022-10-01",
+  //     intervalInDays: 7,
+  //     reminderType: ReminderType.APPOINTMENT,
+  //     description: "RemindesdfsfsdfsdfsdfsdfrTsdofMENT",
+  //     repeat: RepeatType.NEVER
+  //   }
+  // ];
 
   const handleCardClick = (reminder: ReminderItem) => {
     setSelectedReminder(reminder);
@@ -81,22 +95,15 @@ export default function ReminderList() {
         nodeTitle={<span>Reminders</span>}
       />
       <div className="flex flex-1 flex-col bg-app-white">
+        {/* main */}
         <div className="flex flex-col gap-4 p-4">
-          {loading ? (
-            <p>Loading reminders...</p>
-          ) : error ? (
-            <p>Error: {error}</p>
-          ) : reminders.length === 0 ? (
-            <p>No reminders found</p>
-          ) : (
-            reminders.map((reminder) => (
-              <ReminderCard
-                key={reminder.reminderId}
-                reminder={reminder}
-                onClick={() => handleCardClick(reminder)}
-              />
-            ))
-          )}
+          {reminders.map((reminder) => (
+            <ReminderCard
+              key={reminder.reminderId}
+              reminder={reminder}
+              onClick={() => handleCardClick(reminder)}
+            />
+          ))}
         </div>
       </div>
     </div>
